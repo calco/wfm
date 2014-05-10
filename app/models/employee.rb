cbb5cea19fdae2421f823185dd145f2cccda5269
Alaa El-Din Montasser <alaaeldin.montasser@gmail.com>
2014-05-11 00:21:05 +0200

    Style forms. Choose manager by name now.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -21,6 +21,10 @@ class Employee < ActiveRecord::Base
     :case_sensitive => false
   }
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
   # overrides devise's default 'sign in by email only' authentication method
   def self.find_first_by_auth_conditions(warden_conditions)
       conditions = warden_conditions.dup
